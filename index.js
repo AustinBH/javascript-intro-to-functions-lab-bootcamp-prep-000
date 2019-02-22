72a4f7bc78ad899c0c80e2b8de9fa7bf96ce1a62
Austin Harlow <harlowaustin2@gmail.com>
2019-02-22 23:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function whisper(string) {
 function logShout(string) {
   console.log (string.toUpperCase())
 }
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  if (string.isLowerCase()) {
+    return "I can't hear you!"
+}
+else if (string.isUpperCase()) {
+  return "YES INDEED!"
+}
+else if (string === "I love you Grandma") {
+  return "I love you, too."
+}
+}
